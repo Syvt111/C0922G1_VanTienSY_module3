3b2ef9a6e1e5d509b0ee7cc96b3efea2f5be5888
syvt111 <tiensyvan@gmail.com>
2022-12-16 16:55:34 +0700

    nop bai tap

--- CaseStudy/nop_bai_tap/create_table.sql
+++ CaseStudy/nop_bai_tap/create_table.sql
@@ -17,7 +17,7 @@ ten_bo_phan varchar(45)
 ) ;
 
 create table nhan_vien(
-ma_nhan_vien int primary key,
+ma_nhan_vien int ,
 ho_ten varchar(45),
 ngay_sinh date,
 so_cmnd varchar(45),
@@ -27,43 +27,44 @@ email varchar(45),
 dia_chi varchar(45),
 ma_vi_tri int,
 ma_trinh_do int,
-ma_bo_phan int
+ma_bo_phan int,
+primary key(ma_nhan_vien,ma_vi_tri,ma_trinh_do,ma_bo_phan),
+foreign key(ma_vi_tri) references vi_tri(ma_vi_tri),
+foreign key(ma_trinh_do) references trinh_do(ma_trinh_do),
+foreign key(ma_bo_phan)  references bo_phan(ma_bo_phan)
 ) ;
 
+create table loai_khach(
+ma_loai_khach int primary key,
+ten_loai_khach_hang varchar(45)
+);
+
 create table khach_hang(
-ma_khach_hang int primary key,
+ma_khach_hang int ,
 ma_loai_khach int,
+primary key(ma_khach_hang,ma_loai_khach),
 ho_ten varchar(45),
 ngay_sinh date,
 gioi_tinh bit(1),
 so_cmnd varchar(45),
 so_dien_thoai varchar(45),
 email varchar(45),
-dia_chi varchar(45)
-);
-
-create table loai_khach(
-ma_loai_khach_hang int,
-ten_loai_khach_hang varchar(45)
+dia_chi varchar(45),
+foreign key(ma_loai_khach)references loai_khach(ma_loai_khach)
 );
 
 create table loai_dich_vu(
-ma_loai_dich_vu int,
+ma_loai_dich_vu int primary key,
 ten_loai_dich_vu varchar(45)
 );
 
-create table hop_dong(
-ma_hop_dong int,
-ngay_lam_hop_dong datetime,
-ngay_ket_thuc datetime,
-tien_dat_coc double,
-ma_nhan_vien int,
-ma_khach_hang int,
-ma_dich_vu int
+create table kieu_thue(
+ma_kieu_thue int primary key,
+ten_kieu_thue varchar(45)
 );
 
 create table dich_vu(
-ma_dich_vu int,
+ma_dich_vu int  ,
 ten_dich_vu varchar(45),
 dien_tich int,
 chi_phi_thue double,
@@ -74,11 +75,28 @@ tieu_chuan_phong varchar(45),
 mo_ta_tien_nghi_khac varchar(45),
 dien_dich_ho_boi double,
 so_tang int,
-dich_vu_mien_phi_di_kem text
+dich_vu_mien_phi_di_kem text,
+primary key(ma_dich_vu,ma_kieu_thue,ma_loai_dich_vu),
+foreign key(ma_kieu_thue) references kieu_thue(ma_kieu_thue),
+foreign key(ma_loai_dich_vu) references loai_dich_vu (ma_loai_dich_vu)
+);
+
+create table hop_dong(
+ma_hop_dong int,
+ngay_lam_hop_dong datetime,
+ngay_ket_thuc datetime,
+tien_dat_coc double,
+ma_nhan_vien int,
+ma_khach_hang int,
+ma_dich_vu int,
+primary key(ma_hop_dong,ma_nhan_vien,ma_khach_hang,ma_dich_vu),
+foreign key(ma_nhan_vien) references nhan_vien(ma_nhan_vien),
+foreign key(ma_khach_hang) references khach_hang(ma_khach_hang),
+foreign key(ma_dich_vu) references dich_vu(ma_dich_vu)
 );
 
 create table dich_vu_di_kem(
-ma_dich_vu_di_kem int,
+ma_dich_vu_di_kem int primary key,
 ten_dich_vu_di_kem varchar(45),
 gia double,
 don_vi varchar(10),
@@ -89,13 +107,13 @@ create table hop_dong_chi_tiet(
 ma_hop_dong_chi_tiet int,
 ma_hop_dong int,
 ma_dich_vu_di_kem int,
-so_luong int
+primary key(ma_hop_dong_chi_tiet, ma_dich_vu_di_kem, ma_hop_dong),
+so_luong int,
+foreign key(ma_dich_vu_di_kem) references dich_vu_di_kem(ma_dich_vu_di_kem),
+foreign key(ma_hop_dong) references hop_dong(ma_hop_dong)
 );
 
-create table kieu_thue(
-ma_kieu_thue int,
-ten_kieu_thue varchar(45)
-);
+
 
 
 
---
+++ ss3_database _manipulation/bai_tap_1/student_data_management.sql
@@ -0,0 +1,86 @@
+create database if not exists sudent_data_managemnt;
+        use sudent_data_managemnt;
+
+        create table class(
+        class_id   int         not null auto_increment primary key,
+        class_name varchar(60) not null,
+        start_date datetime    not null,
+        status    bit
+        );
+
+        create table student(
+        student_id   int         not null auto_increment primary key,
+        student_name varchar(30) not null,
+        address     varchar(50),
+        phone       varchar(20),
+        status      bit,
+        class_id     int         not null,
+        foreign key (class_id) references class (class_id)
+        );
+
+        create table subject(
+        sub_id   int         not null auto_increment primary key,
+        sub_name varchar(30) not null,
+        credit  tinyint     not null default 1 check ( credit >= 1 ),
+        status  bit                  default 1
+        );
+
+        create table mark (
+        mark_id int not null auto_increment primary key,
+        sub_id int not null,
+        student_id int not null,
+        mark float default 0 check (mark between 0 and 100),
+        exam_times tinyint default 1,
+        unique (sub_id , student_id),
+        foreign key (sub_id)
+        references subject (sub_id),
+        foreign key (student_id)
+        references student (student_id)
+        );
+
+        insert into class
+        values (1, 'a1', '2008-12-20', 1);
+        insert into class
+        values (2, 'a2', '2008-12-22', 1);
+        insert into class
+        values (3, 'b3', current_date, 0);
+
+        insert into student (student_name, address, phone, status, class_id)
+        values ('hung', 'ha noi', '0912113113', 1, 1);
+        insert into student (student_name, address, status, class_id)
+        values ('hoa', 'hai phong', 1, 1);
+        insert into student (student_name, address, phone, status, class_id)
+        values ('manh', 'hcm', '0123123123', 0, 2);
+
+        insert into subject
+        values (1, 'cf', 5, 1),
+        (2, 'c', 6, 1),
+        (3, 'hdj', 5, 1),
+        (4, 'rdbms', 10, 1);
+
+        insert into mark (sub_id, student_id, mark, exam_times)
+        values (1, 1, 8, 1),
+        (1, 2, 10, 2),
+        (2, 1, 12, 1);
+
+        select * from student where student_name like 'h%';
+
+        select * from class where start_date like '%12%';
+
+        select * from subject where credit in (3,4,5);
+
+        set sql_safe_updates = 0 ;
+        update student set class_id = '2' where student_name = 'hùng';
+        select * from student ;
+
+        select
+        s.student_name, sub.sub_name, mark.mark
+        from
+        ((mark
+        join
+        student s
+        on s.student_id = mark.student_id)
+        join subject sub on mark.sub_id = sub.sub_id)
+        order by
+        mark.mark desc, s.student_name asc  ;
+    
